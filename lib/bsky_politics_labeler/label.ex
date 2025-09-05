60817af539a3a7bc691cb08f541e5492e4494a8a
meozk <git@meozk.dev>
2025-09-05 17:13:56 +0300

    Seperate AI to its own module

--- lib/bsky_politics_labeler/label.ex
+++ lib/bsky_politics_labeler/label.ex
@@ -1,11 +1,11 @@
 defmodule BskyPoliticsLabeler.Label do
   require Logger
   alias BskyPoliticsLabeler.Base32Sortable
-  alias BskyPoliticsLabeler.{BskyHttpApi, Post}
+  alias BskyPoliticsLabeler.{BskyHttpApi, Post, GenAi}
 
   def label(post, subject_cid, labeler_did, session_manager) do
     text = BskyHttpApi.get_text(post)
-    is_political = ask_ai(text)
+    is_political = GenAi.ask_ai(text)
     Logger.debug("#{is_political}: #{text}")
 
     if is_political do
@@ -62,67 +62,6 @@ defmodule BskyPoliticsLabeler.Label do
 
       {:error, _} = err ->
         err
-    end
-  end
-
-  def ask_ai(text) do
-    # List available models
-    t0 = System.monotonic_time()
-
-    # This can also be used to measure queue health
-    {:ok, %{data: [%{id: model}]}} =
-      ExOpenAI.Models.list_models(
-        timeout: 120_000,
-        recv_timeout: 120_000
-      )
-
-    content =
-      "\"#{text}\"\nIs the above post about US Politics or about the US legal system? Answer with only Yes or No."
-
-    # Chat completion
-    messages = [
-      %ExOpenAI.Components.ChatCompletionRequestSystemMessage{
-        role: :system,
-        content: "You are a helpful assistant."
-      },
-      %ExOpenAI.Components.ChatCompletionRequestUserMessage{
-        role: :user,
-        content: content
-      }
-    ]
-
-    true = !!Application.get_env(:ex_openai, :base_url)
-
-    ExOpenAI.Chat.create_chat_completion(messages, model,
-      temperature: 0,
-      max_tokens: 3,
-      timeout: 120_000,
-      recv_timeout: 120_000
-    )
-    |> tap(fn _ ->
-      t1 = System.monotonic_time()
-      dur = System.convert_time_unit(t1 - t0, :native, :millisecond) / 1000
-      Logger.debug("Inference took #{dur} s")
-    end)
-    |> case do
-      {:ok, response} ->
-        %ExOpenAI.Components.CreateChatCompletionResponse{
-          choices: [%{message: %{content: answer}}]
-        } =
-          response
-
-        answer = answer |> String.trim(".") |> String.downcase()
-
-        case answer do
-          "no" -> false
-          "yes" -> true
-        end
-
-      {:error, :timeout} ->
-        raise "Inference timeout"
-
-      error ->
-        raise "Inference error: " <> inspect(error)
     end
   end
 end
