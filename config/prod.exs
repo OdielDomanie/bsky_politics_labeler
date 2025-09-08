c65aba4aef44d1db41fb4dc80132a8f049234cdb
meozk <git@meozk.dev>
2025-09-08 14:21:44 +0300

    Fix dashboard bound ip to 0.0.0.0 from localhost

--- config/prod.exs
+++ config/prod.exs
@@ -2,3 +2,7 @@ import Config
 
 config :logger,
   level: :info
+
+# Caution! No HTTPS & Basic Auth means the app must be behind further layers of protection.
+config :bsky_politics_labeler, BskyPoliticsLabeler.WebEndpoint,
+  http: [ip: {0, 0, 0, 0}, port: 4000]
